2fde9e72f7a8bbf9bd4f8241d4408d28a569e7df
Marcelo de la Barrera <mbarrera@capitaria.com>
2022-04-26 17:59:47 -0400

    Se añaden modificaciones

--- 1.-Introduccion/cambiar tipo de dato.js
+++ 1.-Introduccion/cambiar tipo de dato.js
@@ -1,5 +1,5 @@
 let perro = "542";
-const CANTIDAD = parseInt(perro);
+const CANTIDAD = parseInt(perro,10); //Base 10, para dar mayor precision y explicitud.
 console.log(Number(perro), perro);
 let notas = "6.5";
 let nota = parseFloat(notas);
